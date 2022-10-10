123e5cfdbe7cdaef1b164255b99d6ff749d0a3c5
Aiperi Stamakhunova <aiperiseyogmail.com@Aiperis-MacBook-Air.local>
2022-10-10 16:50:33 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+import Card from './components/Cards';
+// import Button from './components/Btn';
+// import Card from './components/Card';
+
+function App() {
+  
+
+  
+  return (
+     <div className="App">
+
+    <Card/>
+    
+
+
+
+      
+    </div>
+  );
+  
+}
+
+export default App;
---
+++ src/components/Cards.js
@@ -0,0 +1,17 @@
+// import { createElement } from "react"
+
+
+function Card(props){
+    return(
+        <div className="container">
+            <div className="Card">
+            <div>Pen</div>
+            <div>$20</div>
+            <div>blue</div>
+            <div>05.08.2022</div>
+            </div>
+        </div>
+    )
+}
+
+export default Card
